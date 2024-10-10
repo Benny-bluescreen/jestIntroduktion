cadddd3931a8ccd40ac047904a0b9e4b5c41500a
Benny Christensen <benny.christensen@millbyte.se>
2024-10-10 19:34:05 +0200

    Fixed linting

--- animal.spec.js
+++ animal.spec.js
@@ -1,50 +1,50 @@
-//Importera animal
+// Importera animal
 const Animal = require("./animal");
 
 describe("Animal", () => {
   
-  let myAnimal
+  let myAnimal;
 
   beforeAll(() => {
     myAnimal = new Animal("a", 1, "ape");
-  })
+  });
 
-  //Before each
+  // Before each
   beforeEach(() => {
-    myAnimal.name = "a"
-    myAnimal.age = 1
-    myAnimal.species = "ape"
- })
+    myAnimal.name = "a";
+    myAnimal.age = 1;
+    myAnimal.species = "ape";
+  });
 
   it("Skapa Animal object", () => {        
-    expect(myAnimal.name).toBe("a")
-    expect(myAnimal.age).toBe(1)
-    expect(myAnimal.species).toBe("ape")
-  })
+    expect(myAnimal.name).toBe("a");
+    expect(myAnimal.age).toBe(1);
+    expect(myAnimal.species).toBe("ape");
+  });
 
   it("Aging", () => {
-    expect(myAnimal.age).toBe(1)
-    myAnimal.aging()
-    expect(myAnimal.age).toBe(2)
-  })
+    expect(myAnimal.age).toBe(1);
+    myAnimal.aging();
+    expect(myAnimal.age).toBe(2);
+  });
 
   it("Renaming done correct", () => {
-    myAnimal.rename("Adam")
-    expect(myAnimal.name).toBe("Adam")
+    myAnimal.rename("Adam");
+    expect(myAnimal.name).toBe("Adam");
 
-    myAnimal.rename("Beatrice")
-    expect(myAnimal.name).toBe("Beatrice")
-  })
+    myAnimal.rename("Beatrice");
+    expect(myAnimal.name).toBe("Beatrice");
+  });
 
   it("Wrongful renaming", () => {
-    //Spara errormeddelade i var
-    let response = myAnimal.rename("Andxxxers")
-    expect(response).toMatch("Felaktigt namn")
-    expect(myAnimal.name).toBe("a")
-  })
+    // Spara errormeddelade i var
+    let response = myAnimal.rename("Andxxxers");
+    expect(response).toMatch("Felaktigt namn");
+    expect(myAnimal.name).toBe("a");
+  });
 
-  it("ToString metod", () => {
-    let response = myAnimal.toString()
-    expect(response).toMatch("Denna ape heter a och är 1 år gammal.")
-  })
-})
+  it("ToString method", () => {
+    let response = myAnimal.toString();
+    expect(response).toMatch("Denna ape heter a och är 1 år gammal.");
+  });
+});
