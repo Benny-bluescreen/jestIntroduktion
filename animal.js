0161eabc2803c000ebc855267252465837efb892
Benny Christensen <benny.christensen@millbyte.se>
2024-10-11 09:04:05 +0200

    Linting problems

--- animal.js
+++ animal.js
@@ -1,24 +1,24 @@
 class Animal {
     constructor(name, age, species) {
-        this.name = name
-        this.age = age
-        this.species = species
+        this.age = age;
+        this.species = species;
+        this.name = name;
     }
 
     aging() {
-        this.age++
+        this.age++;
     }
 
     toString() {
-        return (`Denna ${this.species} heter ${this.name} och är ${this.age} år gammal.`)
+        return `Denna ${this.species} heter ${this.name} och är ${this.age} år gammal.`;
     }
 
     rename(newName) {
-        if (newName.includes("xxx")) return "Felaktigt namn"
+        if (newName.includes("xxx")) return "Felaktigt namn";
 
-        this.name = newName
+        this.name = newName;
     }
 }
 
-//Exportera klass
-module.exports = Animal
+// Exportera klass
+module.exports = Animal;
